2580ce2c1b4f1efc275907a748833df4232d9da4
nurulsucisawaliah <71654996+nurulsucisawaliah@users.noreply.github.com>
2020-11-09 05:22:03 +0800

    Create latihan01(praktik 1).py

---
+++ latihan soal 1(PRAKTIKUM 1)/latihan01(praktik 1).py
@@ -0,0 +1,11 @@
+bahasaindonesia =int(input('Nilai Bahasa Indonesia :'))
+ipa             =int(input('Nilai ipa              :'))
+matematika      =int(input('Nilai Matematika       :'))
+if(bahasaindonesia >60 and ipa>60 and matematika >70):
+      print("statusKelulusan = Lulus")
+else:
+     print("statusKelulusan = Tidak Lulus")
+    
+
+   
+
