8ae143f062112b7e5e39c9400c7429db12d0af73
nurulsucisawaliah <71654996+nurulsucisawaliah@users.noreply.github.com>
2020-11-09 05:22:31 +0800

    Create latihan soal nomor 4.py

---
+++ latihan soal 4(PRAKTIKUM 2)/latihan soal nomor 4.py
@@ -0,0 +1,14 @@
+#kotak bintang ajaib
+#Modifikasilah kode program dari langkah kerja 10
+
+kolom = 6
+baris = 5
+
+i = 0
+while (i < baris):
+    j = i + 1
+    while (j < kolom):
+        print('* ', end='')
+        j += 1
+    print('')
+    i += 1
