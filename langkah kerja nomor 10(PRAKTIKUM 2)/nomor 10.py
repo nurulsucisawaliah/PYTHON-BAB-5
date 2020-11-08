1d3e5f41ec7c9c883f58fc497927e65e98452ad7
nurulsucisawaliah <71654996+nurulsucisawaliah@users.noreply.github.com>
2020-11-09 05:21:29 +0800

    Create nomor 10.py

---
+++ langkah kerja nomor 10(PRAKTIKUM 2)/nomor 10.py
@@ -0,0 +1,14 @@
+#Modifikasilah kode program dari nomor 8
+#kotak bintang ajaib
+
+baris =6
+kolom =0
+i = 0
+while(i < baris):
+    j =0
+    while(j < kolom):
+        print('* ', end='')
+        j +=1
+    print('')
+    i +=1
+    kolom += 1
