8b4e534f43d0169333d216480ca9dcbad5bd6854
nurulsucisawaliah <71654996+nurulsucisawaliah@users.noreply.github.com>
2020-11-09 05:21:38 +0800

    Create nomor 13.py

---
+++ langkah kerja nomor 13(PRAKTIKUM 2)/nomor 13.py
@@ -0,0 +1,10 @@
+#modifikasi kode program nomor 11
+from random import randint
+banyak = 0
+while True:
+    bil = randint(0,10)
+    print(bil)
+    banyak = banyak + 1
+    if bil == 5:
+        break
+print('banyak perulangan : ' + str(banyak))
